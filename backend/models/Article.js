f7b4123d3c08a882be1f6632479491b18d356d9b
Jonathan Mardones <jmardones.94@gmail.com>
2021-10-04 23:21:31 -0300

    initial state

---
+++ backend/models/Article.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const articleSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  brand: { type: String, required: true },
+  price: { type: Number, required: true },
+  hasDiscount: { type: Boolean, default: false },
+  discountPrice: { type: Number, required: false },
+  photos: [{ type: String }], // revisar. quizá cambiar a File
+  categories: [{ type: String }],
+  gameType: { type: String }, // de rol, de mesa, etc. Revisar otros tipos de artículos: mangas, dados, muñecos, etc.
+  languages: [{ type: String }], // revisar mañana. array o un solo valor?
+  minPlayers: { type: Number },
+  maxPlayers: { type: Number },
+  minAge: { type: Number },
+  stock: { type: Number, required: true },
+  size: { type: String, required: false }, // revisar
+  weight: { type: String, required: false },
+})
+
+const Article = mongoose.model("article", articleSchema)
+
+module.exports = Article
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,30 @@
 const mongoose = require("mongoose")
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  photo: { type: String, required: true }, // cambiar por tipo de dato File.
+  phone: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+  google: { type: Boolean, default: false },
+  directions: [
+    {
+      street: { type: String, required: true },
+      number: { type: Number, required: true },
+      department: { type: String, required: true },
+      zipCode: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+    },
+  ],
+  wishList: [{ type: mongoose.Types.ObjectId, ref: "article" }],
+  purchaseHistory: [{ type: mongoose.Types.ObjectId, ref: "article" }],
+  createdAt: { type: Date, default: Date.now() },
+  passwordChangesHistory: [
+    { oldPassword: { type: String }, date: { type: Date } },
+  ],
+})
 
 const User = mongoose.model("user", userSchema)
 
