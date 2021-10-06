a881ef0d0a87ba1eca092f6acf5919142a0c05b6
Jonathan Mardones <jmardones.94@gmail.com>
2021-10-06 12:00:34 -0300

    ahora si jj

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,18 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import { useLoginLS } from "./hooks/usersHooks"
+import { useSelector } from "react-redux"
 
 const App = (props) => {
   useLoginLS()
+  const user = useSelector((state) => state.users.user)
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/articles" component={Articles} />
-        {!props.token && <Route path="/signin" component={SignIn} />}
-        {!props.token && <Route path="/signup" component={SignUp} />}
+        {!user && <Route path="/signin" component={SignIn} />}
+        {!user && <Route path="/signup" component={SignUp} />}
         <Redirect to="/" />
       </Switch>
     </BrowserRouter>
