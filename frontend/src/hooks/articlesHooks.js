0f2ac76358a457e5bb0ac9ea3d30454a6e0f0d0b
Franco Quattrini <fquattrini01@gmail.com>
2021-10-06 22:22:45 -0300

    diganAntes

--- frontend/src/hooks/articlesHooks.js
+++ frontend/src/hooks/articlesHooks.js
@@ -3,44 +3,44 @@ import { useState, useEffect } from "react"
 import articlesActions from "../redux/actions/articlesActions"
 
 export const useArticles = () => {
-  const [articles, setArticles] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const dispatch = useDispatch()
+   const [articles, setArticles] = useState([])
+   const [loading, setLoading] = useState(true)
+   const [error, setError] = useState(null)
+   const dispatch = useDispatch()
 
-  useEffect(() => {
-    fetchArticles()
-  }, [])
+   useEffect(() => {
+      fetchArticles()
+   }, [])
 
-  const fetchArticles = async () => {
-    const res = await dispatch(articlesActions.getArticles)
-    if (!res.success) setError(res.error)
-    setLoading(false)
-    setArticles(res.response)
-  }
-  return [articles, loading, error]
+   const fetchArticles = async () => {
+      const res = await dispatch(articlesActions.getArticles)
+      if (!res.success) setError(res.error)
+      setLoading(false)
+      setArticles(res.response)
+   }
+   return [articles, loading, error]
 }
 
 export const useArticle = (id) => {
-  const [article, setArticle] = useState({})
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+   const [article, setArticle] = useState({})
+   const [loading, setLoading] = useState(true)
+   const [error, setError] = useState(null)
+   const dispatch = useDispatch()
+   useEffect(() => {
+      setLoading(true)
+      setError(null)
+      fetchArticle()
+   }, [id])
 
-  useEffect(() => {
-    setLoading(true)
-    setError(null)
-    fetchArticle()
-  }, [id])
+   const fetchArticle = async () => {
+      const res = await dispatch(articlesActions.getArticle(id))
+      if (!res.success) {
+         setError(res.error)
+      } else {
+         setArticle(res.response)
+      }
+      setLoading(false)
+   }
 
-  const fetchArticle = async () => {
-    const res = await dispatch(articlesActions.getArticle(id))
-    if (!res.success) {
-      setError(res.error)
-    } else {
-      setArticle(res.response)
-    }
-    setLoading(false)
-  }
-
-  return [article, loading, error]
+   return [article, loading, error]
 }
