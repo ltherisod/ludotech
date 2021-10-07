4a15b669c70ab6f0f907fd105acd30fc9de29171
unknown <meriquiroga@hotmail.com>
2021-10-07 12:05:29 -0300

    Filter

--- frontend/src/pages/Article.js
+++ frontend/src/pages/Article.js
@@ -24,6 +24,9 @@ const Article = () => {
             <div className="articleDecoDown">
             <div className="articleDeco" style={{backgroundImage: `url("https://i.postimg.cc/5NmMNsQM/codex-Deco4.png")`}}></div>
             </div>
+            <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/3Ln6yqmoCpE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
             <Footer/>
         </div>
     )
