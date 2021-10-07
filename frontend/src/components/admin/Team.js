c749504f4adeac0c2ed281a98e5bf0026b65fe75
Sebastian Campos <bastiampos.uni@gmail.com>
2021-10-07 02:44:13 -0300

    team panel admin

--- frontend/src/components/admin/Team.js
+++ frontend/src/components/admin/Team.js
@@ -10,13 +10,34 @@ const Team = () => {
 
     useEffect(() => {
         axios.get('http://localhost:4000/api/admin')
-            .then(res=> setMembers(res.data.response))
+            .then(res=> {
+                setMembers(res.data.response)
+            })
             .catch(e => console.log(e))
     },[])
 
     const getUser = () => {
         axios.post('http://localhost:4000/api/admin', {email: search})
-            .then(res => setUserFound(res.data.response))
+            .then(res => {
+                console.log(res.data)
+                if(res.data.response) {
+                    setUserFound(res.data.response)
+                    setNotFound(false)
+                } else {
+                    setNotFound(true)
+                }
+            })
+            .catch(e => {
+                console.log(e)
+                setNotFound(true)
+            })
+    }
+
+    const changeRol = () => {
+        axios.post('http://localhost:4000/api/admin/set-admin', {email: userFound.email})
+            .then(res => {
+                setUserFound(false)
+            })
             .catch(e => console.log(e))
     }
 
@@ -34,13 +55,13 @@ const Team = () => {
                     </svg>
                 </div>
                 {userFound && <div className='userFoundTeam'>
-                    <div className='userPicFoundTeam' style={{backgroundImage: `url("https://cdn.dribbble.com/users/642793/screenshots/8811037/media/4aa0f2c95adadfd856c0c0397689d65b.jpg?compress=1&resize=1200x900")`}}></div>
+                    <div className='userPicFoundTeam' style={{backgroundImage: `url("${userFound.photo}")`}}></div>
                     <div className='detailsUserFound'>
                         <div>
                             <label className='bold'>Fullname</label>
-                            <p>Hashima Yurobi</p>
+                            <p>{userFound.firstname} {userFound.lastname}</p>
                             <label className='bold'>Email</label>
-                            <p>hashima@gmail.com</p>
+                            <p>{userFound.email}</p>
                         </div>
                         <div>
                             <div>
@@ -49,7 +70,10 @@ const Team = () => {
                             </div>
                             <div>
                                 <label className='bold'>New rol:</label>
-                                <select><option>Admin</option><option>User</option></select>
+                                <div style={{display: 'flex', alignItems: 'center'}}>
+                                    <select><option>Admin</option><option>User</option></select>
+                                    <div onClick={() => changeRol()} className='saveTeam'>Save</div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -59,62 +83,25 @@ const Team = () => {
             <div>
                 <p className='subtitleTeam'>Members</p>
                 <div className='cardsTeam'>
-                    <div className='cardTeam'>
-                        <div className='picCardTeam' style={{backgroundImage: `url("https://cdn.dribbble.com/users/913447/screenshots/15233866/media/f9d66c4fae205ccd368a1c618d4f88c7.jpg?compress=1&resize=800x600")`}}>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                </svg>
+                    {members && members.map( member => {
+                        return (
+                            <div key={member._id} className='cardTeam'>
+                                <div className='picCardTeam' style={{backgroundImage: `url("${member.photo}")`}}>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className='detailsCardTeam'>
+                                    <p>{member.firstname} {member.lastname}</p>
+                                    <span>{member.email}</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className='detailsCardTeam'>
-                            <p>Sebastian Campos</p>
-                            <span>bastiampos@gmail.com</span>
-                        </div>
-                    </div>
-                    <div className='cardTeam'>
-                        <div className='picCardTeam' style={{backgroundImage: `url("https://cdn.dribbble.com/users/913447/screenshots/15233866/media/f9d66c4fae205ccd368a1c618d4f88c7.jpg?compress=1&resize=800x600")`}}>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div className='detailsCardTeam'>
-                            <p>Sebastian Campos</p>
-                            <span>bastiampos@gmail.com</span>
-                        </div>
-                    </div>
-                    <div className='cardTeam'>
-                        <div className='picCardTeam' style={{backgroundImage: `url("https://cdn.dribbble.com/users/913447/screenshots/15233866/media/f9d66c4fae205ccd368a1c618d4f88c7.jpg?compress=1&resize=800x600")`}}>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div className='detailsCardTeam'>
-                            <p>Sebastian Campos</p>
-                            <span>bastiampos@gmail.com</span>
-                        </div>
-                    </div>
-                    <div className='cardTeam'>
-                        <div className='picCardTeam' style={{backgroundImage: `url("https://cdn.dribbble.com/users/913447/screenshots/15233866/media/f9d66c4fae205ccd368a1c618d4f88c7.jpg?compress=1&resize=800x600")`}}>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div className='detailsCardTeam'>
-                            <p>Sebastian Campos</p>
-                            <span>bastiampos@gmail.com</span>
-                        </div>
-                    </div>
+                        )
+                    })}                   
+                    
                 </div>
             </div>
         </div>
--- frontend/src/pages/PanelAdmin.js
+++ frontend/src/pages/PanelAdmin.js
@@ -1,65 +1,21 @@
 import React, { useState } from "react"
-import { useFormik } from "formik"
-import Header from "../components/Header"
 import ArticlesAdmin from "../components/admin/ArticlesAdmin"
+import NavBar from "../components/admin/NavBar"
 import Team from "../components/admin/Team"
 
-const PanelAdmin = () => {
-   const [showDashboard, setShowDashboard] = useState(false)
-   const [showArticle, setShowArticle] = useState(false)
-   const [showSales, setShowSales] = useState(false)
-   const [showUsers, setShowUsers] = useState(false)
+const PanelAdmin = (props) => {
+   const [show, setShow] = useState('dashboard')
+
+   const render = (page) => {
+      setShow(page)
+   }
+
    return (
-      <>
-         <Header />
-         <div
-            className="body"
-            style={{
-               backgroundImage: `url("https://i.postimg.cc/3wVXYt59/back-Ludo3.png")`,
-            }}
-         >
-            <div
-               className="hero"
-               style={{
-                  backgroundImage: `url("https://i.postimg.cc/QdTXy2Wx/herofixed.png")`,
-               }}
-            >
-               <div className="adminPanel">
-                  <h1>ADMIN PANEL</h1>
-                  <button
-                     type="button"
-                     className="adminPanelButton"
-                     onClick={() => setShowDashboard(!showDashboard)}
-                  >
-                     Dashboard
-                  </button>
-                  <button
-                     type="button"
-                     className="adminPanelButton"
-                     onClick={() => setShowArticle(!showArticle)}
-                  >
-                     Articles
-                  </button>
-                  <button
-                     type="button"
-                     className="adminPanelButton"
-                     onClick={() => setShowSales(!showSales)}
-                  >
-                     Sales
-                  </button>
-                  <button
-                     type="button"
-                     className="adminPanelButton"
-                     onClick={() => setShowUsers(!showUsers)}
-                  >
-                     Users
-                  </button>
-               </div>
-               {showArticle && <ArticlesAdmin />}
-               {showUsers && <Team />}
-            </div>
-         </div>
-      </>
+      <div style={{display: 'flex'}}>
+         <NavBar render={render} />
+         {(show === 'articles') && <ArticlesAdmin />}
+         {(show === 'team') && <Team />}
+      </div>
    )
 }
 
