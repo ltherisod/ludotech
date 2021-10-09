cc82eabbd06c2fe318c86d58a95ad01424e81faa
Franco Quattrini <fquattrini01@gmail.com>
2021-10-09 16:14:43 -0300

    profile-address-add-edit-delete-ready

---
+++ frontend/src/components/Address.js
@@ -0,0 +1,84 @@
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import usersActions from "../redux/actions/usersActions"
+import DirectionsForm from "./DirectionsForm"
+
+const Address = (props) => {
+   const dispatch = useDispatch()
+   const [loading, setLoading] = useState(false)
+   const [error, setError] = useState(null)
+   const [edit, setEdit] = useState(false)
+   const { direction } = props
+
+   const updateDirectionHandler = async (values) => {
+      setLoading(true)
+      const res = await dispatch(
+         usersActions.updateDirection(values, direction?._id)
+      )
+      console.log(res)
+      if (!res.success) setError(res.error)
+      setLoading(false)
+   }
+
+   const initialValues = {
+      alias: direction?.alias,
+      receiver: direction?.receiver,
+      street: direction?.street,
+      number: direction?.number,
+      department: direction?.department,
+      zipCode: direction?.zipCode,
+      city: direction?.city,
+      state: direction?.state,
+   }
+
+   const deleteAddress = async (id) => {
+      const res = await dispatch(usersActions.deleteDirection(id))
+      console.log(res)
+   }
+
+   return (
+      <div>
+         {loading ? (
+            <h1>LOADING PADREE!!!</h1>
+         ) : (
+            <div>
+               {!edit && (
+                  <div>
+                     <p>Alias : {direction.alias}</p>
+                     <p>Receiver : {direction.receiver}</p>
+                     <p>Street : {direction.street}</p>
+                     <p>Number : {direction.number}</p>
+                     <p>Department : {direction.department}</p>
+                     <p>Zip Code : {direction.zipCode}</p>
+                     <p>City : {direction.city}</p>
+                     <p>State : {direction.state}</p>
+                  </div>
+               )}
+               {edit && (
+                  <div>
+                     <DirectionsForm
+                        submitCallback={updateDirectionHandler}
+                        initialValues={initialValues}
+                        buttonText="Confirm"
+                     />
+                     <button type="button" onClick={() => setEdit(!edit)}>
+                        Cancel
+                     </button>
+                  </div>
+               )}
+               <button type="button" onClick={() => setEdit(!edit)}>
+                  Edit Address
+               </button>
+               <button
+                  type="button"
+                  onClick={() => deleteAddress(direction._id)}
+               >
+                  Delete Address
+               </button>
+            </div>
+         )}
+      </div>
+   )
+}
+
+export default Address
--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -1,33 +1,104 @@
+import { useState } from "react"
 import { useSelector } from "react-redux"
+import DirectionsForm from "../components/DirectionsForm"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import HeroPages from "../components/HeroPages"
+import usersActions from "../redux/actions/usersActions"
+import { useDispatch } from "react-redux"
+import Address from "../components/Address"
 
 const UserProfile = () => {
    const user = useSelector((state) => state.users.user)
    const { directions, email, firstname, lastname, photo, phone } = user
-   console.log(user)
+   const [visibleDirectionForm, setVisibleDirectionForm] = useState(false)
+   const dispatch = useDispatch()
+   const [loading, setLoading] = useState(false)
+   const [error, setError] = useState(null)
+
+   const addDirectionHandler = async (values) => {
+      setLoading(true)
+      const res = await dispatch(usersActions.addDirection(values))
+      console.log(res)
+      if (!res.success) setError(res.error)
+      setLoading(false)
+   }
+
+   const initialValues = {
+      alias: "",
+      receiver: "",
+      street: "",
+      number: "",
+      department: "",
+      zipCode: "",
+      city: "",
+      state: "",
+   }
+
    return (
       <>
          <Header />
          <div
-            className="signInBody"
+            className="backProfile"
             style={{
                backgroundImage: `url("https://i.postimg.cc/3wVXYt59/back-Ludo3.png")`,
             }}
          >
             <HeroPages />
-            <div className="flex">
-               <div className="main-sign">
+            <div className="containerProfile">
+               <div className="flex">
                   <div
                      className="logoUserProfile"
                      style={{
                         backgroundImage: `url('${photo}')`,
                      }}
                   ></div>
-                  <h2>
-                     {firstname} {lastname}
-                  </h2>
+               </div>
+               <div>
+                  <div>
+                     <h2>
+                        {firstname} {lastname}
+                     </h2>
+                     <p>{email}</p>
+                     <div className="d-flex">
+                        <p>{phone ? phone : "No phone added"}</p>
+                        <button type="button">Add here</button>
+                     </div>
+                  </div>
+                  <div>
+                     <h3>Addresses</h3>
+                     <div>
+                        {!directions || directions.length === 0 ? (
+                           <p>There are no addresses added</p>
+                        ) : (
+                           directions.map((direction) => {
+                              return (
+                                 <Address
+                                    direction={direction}
+                                    key={direction._id}
+                                 />
+                              )
+                           })
+                        )}
+                     </div>
+                     <div className="d-flex">
+                        <button
+                           type="button"
+                           onClick={() =>
+                              setVisibleDirectionForm(!visibleDirectionForm)
+                           }
+                        >
+                           Add Address
+                        </button>
+                     </div>
+                     {visibleDirectionForm && (
+                        <DirectionsForm
+                           submitCallback={addDirectionHandler}
+                           initialValues={initialValues}
+                           buttonText="ADD"
+                        />
+                     )}
+                  </div>
                </div>
             </div>
          </div>
