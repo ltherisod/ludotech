436a804089727a4dd4c1ea9249a0a417401faaf2
unknown <meriquiroga@hotmail.com>
2021-10-10 13:12:40 -0300

    Mobile cart

--- mobile/components/MostWanted.js
+++ mobile/components/MostWanted.js
@@ -8,34 +8,75 @@ import {
   StyleSheet,
   Dimensions,
 } from "react-native";
+import {useEffect, useState} from 'react'
+import { connect } from "react-redux"
+import articlesActions from "../redux/actions/articlesActions";
 
-const MostWanted = () => {
+const MostWanted = (props) => {
+    const [mostVisitArticles, setMostVisitArticles] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        props.getMostVisitArticles()
+        .then(res => {
+            setMostVisitArticles(res.response)
+            setLoading(false)
+        })
+        .catch(e => console.log(e))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
   return (
-    <View>
+    /* <View>
       <Text style={styles.mainText}>Most wanted products</Text>
       <View>
       <ImageBackground style={styles.cardImg} source={{ uri: "https://i.postimg.cc/gJb32QpL/card2.png" }}>
-        <ImageBackground style={styles.gameImg} source={{ uri: "https://i.postimg.cc/yNH9CpQ5/King-Of-Tokyo2016-caja-web.png", }}></ImageBackground>
-        <Text style={styles.gameName}>King of Tokio</Text>
+        <ImageBackground style={styles.gameImg} source={{ uri: mostVisitArticles[0].photos[0] }}></ImageBackground>
+        <Text style={styles.gameName}>{mostVisitArticles[0].name}</Text>
       </ImageBackground>
       </View>
       <View>
       <ImageBackground style={styles.cardImg} source={{ uri: "https://i.postimg.cc/gJb32QpL/card1.png" }}>
-        <ImageBackground style={styles.gameImg} source={{ uri: "https://i.postimg.cc/RZsQ5xJ0/pandemic-Cthulhu.png", }}></ImageBackground>
-        <Text style={styles.gameName}>Pandemic Cthulhu</Text>
+        <ImageBackground style={styles.gameImg} source={{ uri: mostVisitArticles[1].photos[0] }}></ImageBackground>
+        <Text style={styles.gameName}>{mostVisitArticles[1].name}</Text>
       </ImageBackground>
       </View>
       <View>
       <ImageBackground style={styles.cardImg} source={{ uri: "https://i.postimg.cc/gJb32QpL/card2.png" }}>
-        <ImageBackground style={styles.gameImg} source={{ uri: "https://i.postimg.cc/fT9jtmhZ/exploding-Kittens-Icon.png", }}></ImageBackground>
-        <Text style={styles.gameName}>Exploding Kittens</Text>
+        <ImageBackground style={styles.gameImg} source={{ uri: mostVisitArticles[2].photos[0]}}></ImageBackground>
+        <Text style={styles.gameName}>{mostVisitArticles[2].name}</Text>
       </ImageBackground>
       </View>
-    </View>
+    </View> */
+
+<View>
+<Text style={styles.mainText}>Most wanted products</Text>
+<View>
+<ImageBackground style={styles.cardImg} source={{ uri: "https://i.postimg.cc/gJb32QpL/card2.png" }}>
+  <ImageBackground style={styles.gameImg} source={{ uri: "https://i.postimg.cc/yNH9CpQ5/King-Of-Tokyo2016-caja-web.png", }}></ImageBackground>
+  <Text style={styles.gameName}>King of Tokio</Text>
+</ImageBackground>
+</View>
+<View>
+<ImageBackground style={styles.cardImg} source={{ uri: "https://i.postimg.cc/gJb32QpL/card1.png" }}>
+  <ImageBackground style={styles.gameImg} source={{ uri: "https://i.postimg.cc/RZsQ5xJ0/pandemic-Cthulhu.png", }}></ImageBackground>
+  <Text style={styles.gameName}>Pandemic Cthulhu</Text>
+</ImageBackground>
+</View>
+<View>
+<ImageBackground style={styles.cardImg} source={{ uri: "https://i.postimg.cc/gJb32QpL/card2.png" }}>
+  <ImageBackground style={styles.gameImg} source={{ uri: "https://i.postimg.cc/fT9jtmhZ/exploding-Kittens-Icon.png", }}></ImageBackground>
+  <Text style={styles.gameName}>Exploding Kittens</Text>
+</ImageBackground>
+</View>
+</View>
   );
 };
 
-export default MostWanted;
+const mapDispatchToProps = {
+  getMostVisitArticles: articlesActions.getMostVisitArticles,
+}
+
+export default connect(null, mapDispatchToProps)(MostWanted)
 
 const styles = StyleSheet.create({
   mainText: {
--- mobile/screens/Cart.js
+++ mobile/screens/Cart.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { connect } from "react-redux"
 import articlesActions from "../redux/actions/articlesActions"
-import {View, Text, TouchableOpacity, Image, SafeAreaView, ScrollView, Platform, StatusBar, StyleSheet, Dimensions} from "react-native"
+import {View, Text, TouchableOpacity, Image, SafeAreaView, ScrollView, Platform, StatusBar, StyleSheet, Dimensions, ImageBackground} from "react-native"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import HeroPages from "../components/HeroPages"
 
 const Cart = (props) => {
     let totalCost = props.shoppingCart.reduce((count, item) => {
@@ -35,36 +36,44 @@ const Cart = (props) => {
     return (
         <SafeAreaView style={{marginTop: Platform.OS === "android" && StatusBar.currentHeight, flex: 1}}>
             <ScrollView>
-                <Header/>
+                {/* <Header/> */}
+                <View>
+                    <ImageBackground style={styles.mainImg} source={{ uri: "https://i.postimg.cc/0Nz37Ydx/fondo-Blanco.png" }}>
+                <HeroPages />
                 <View style={styles.CartMain}>
-                    <Text>Cart</Text>
+                    <Text style={styles.cartTitle}>Cart</Text>
                     <View>
-                        <View>
-                            <Text>Product</Text>
-                            <Text>Delete</Text>
+                        {/* <View>
+                            
                             <Text>Quantity</Text>
                             <Text>Price</Text>
                             <Text>Subtotal</Text>
-                        </View>
+                        </View> */}
                         <View>
                         {props.shoppingCart.map((article) => {
                             return (
                                 <View key={article.article._id}>
-                                    <Image source={{uri: article.article.photos[0]}}/>
-                                    <Text>{article.article.name}</Text>
-                                    <TouchableOpacity onPress={() => {updateCartFunction("delete", article.article._id)}}>
-                                        <Text>x</Text>
+                                    <View style={styles.firstLine}>
+                                    <Image style={styles.productImg} source={{uri: article.article.photos[0]}}/>
+                                    <Text style={styles.productName}>{article.article.name}</Text>
+                                    <TouchableOpacity style={styles.delete} onPress={() => {updateCartFunction("delete", article.article._id)}}>
+                                        <Text style={styles.deleteText}>x</Text>
                                     </TouchableOpacity>
-                                    <View>
-                                        <TouchableOpacity onPress={() => {updateCartFunction("increment", article.article._id)}}>
-                                            <Text>+</Text>
+                                    </View>
+
+                                    <View style={styles.secondLine}>
+                                    <View style={styles.secondLineView}>
+                                    <Text style={styles.innerTitle}>Quantity:</Text>    
+                                        <TouchableOpacity style={styles.quantity} onPress={() => {updateCartFunction("decrement", article.article._id)}}>
+                                            <Text style={styles.quantityText}>{'<'}</Text>
                                         </TouchableOpacity>
                                         <Text>{article.quantity}</Text>
-                                        <TouchableOpacity onPress={() => {updateCartFunction("decrement", article.article._id)}}>
-                                            <Text>-</Text>
+                                        <TouchableOpacity style={styles.quantity} onPress={() => {updateCartFunction("increment", article.article._id)}}>
+                                            <Text style={styles.quantityText}>{'>'}</Text>
                                         </TouchableOpacity>
                                     </View>
-                                    <View>
+                                    <View style={styles.secondLineView}>
+                                    <Text style={styles.innerTitle}>Price:</Text>    
                                     {article.article.hasDiscount === false ? (
                                         <Text>${article.article.price}</Text>
                                     ) : (
@@ -74,20 +83,25 @@ const Cart = (props) => {
                                         </>
                                     )}
                                     </View>
+
+                                    </View>
+                                    <View style={styles.thirdLine}>
+                                    <Text style={styles.innerTitle}>Subtotal:</Text>
                                     <Text>${article.quantity*(article.article.hasDiscount ? article.article.discountPrice : article.article.price)}</Text>
+                                    </View>
                                 </View>
                             )
                         })}
                         </View>
                     </View>
-                    <View>
+                    <View style={styles.totalsCard}>
                         <View>
-                            <View>
-                                <Text>Total without discounts:</Text>
+                            <View style={styles.totals}>
+                                <Text style={styles.totalsText}>Total without discounts:</Text>
                                 <Text>${totalWithoutDiscounts}</Text>
                             </View>
-                            <View>
-                                <Text>Total with discounts:</Text>
+                            <View style={styles.totals}>
+                                <Text style={styles.totalsText}>Total with discounts:</Text>
                                 <Text>${totalCost}</Text>
                             </View>
                         </View>
@@ -96,7 +110,10 @@ const Cart = (props) => {
                         </TouchableOpacity>
                     </View>
                 </View>
-                <Footer/>
+                <Footer />
+                    </ImageBackground>
+                </View>
+                
             </ScrollView>
         </SafeAreaView>
     )
@@ -118,5 +135,94 @@ const {height} = Dimensions.get("window")
 const styles = StyleSheet.create({
     CartMain: {
         minHeight: height-StatusBar.currentHeight,
+        alignItems: 'center'
     },
+    mainImg: {
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+      },
+      cartTitle: {
+        fontFamily: "Poppins_700Bold",
+        fontSize: 25,
+      },
+      productImg: {
+          width: 100,
+          height: 100
+      },
+      productTitle: {
+        paddingRight: 10,
+        fontSize: 18,
+        paddingTop: 15
+      },
+      firstLine: {
+          flexDirection: 'row',
+          width: 380,
+          justifyContent: 'space-between',
+          alignItems: 'center',
+      },
+      productName: {
+        fontSize: 18,
+        textAlign: 'left',
+        width: '60%'
+      },
+      delete: {
+          backgroundColor: 'rgb(160, 160, 160)',
+          paddingTop: 5,
+          paddingBottom: 7,
+          paddingHorizontal: 10,
+          borderRadius: 100
+      },
+      deleteText: {
+          color: 'white'
+      },
+      secondLine: {
+        flexDirection: 'row',
+        width: 380,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    secondLineView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    thirdLine: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    quantity: {
+        color: 'white',
+        backgroundColor: '#6aefcf',
+        paddingTop: 5,
+        paddingBottom: 7,
+        paddingHorizontal: 10,
+        borderRadius: 100,
+        margin: 5
+    },
+    quantityText: {
+        color: 'white'
+    },
+    innerTitle: {
+        paddingRight: 10,
+        fontSize: 18
+    },
+    totalsCard: {
+        marginTop: 30,
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 10,
+        shadowColor: 'gray',
+        shadowOpacity: 10,
+        width: 380,
+    },
+    totals: {
+        flexDirection: 'row',
+        padding: 5
+    },
+    totalsText: {
+        paddingRight: 5
+    }
 })
