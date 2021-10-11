f6b0c7ef35ea6024909c94a908ab5ce768f6b587
Juan Eduardo Castro Trujillo <jc.venepro@gmail.com>
2021-10-11 13:00:17 -0300

    mobile varios ok

--- mobile/screens/Articles.js
+++ mobile/screens/Articles.js
@@ -6,45 +6,48 @@ import Filter from "../components/Filter"
 import HeroPages from "../components/HeroPages"
 
 const Articles = (props) => {
+
     const [articles, setArticles] = useState({
         articles: [],
         page: 1,
         totalCounts: null,
         totalPages: 1,
     })
+
     const [currentPage, setCurrentPage] = useState(1)
+
     const filterArticles = (e) => {
         setArticles(e)
     }
     
     return (
-            <ScrollView>
-                <ImageBackground style={{width:"100%"}}source={require("../assets/fondoVioleta.png")} resizeMode="cover">
-                    <HeroPages/>
-                    <View style={styles.articlesMain}>
-                        <Text style={styles.articlesTitle}>Articles</Text>
-                        <Filter
-                            filterArticles={(e) => filterArticles(e)}
-                            currentPage={currentPage}
-                            setCurrentPage={setCurrentPage}
-                        /> 
-                        <View>
-                        {articles.articles.map((article) => {
-                            return (
-                                <TouchableOpacity onPress={() => props.navigation.navigate('ArticleStack', { id: article._id })}>
-                                    <Article
-                                        history={props.history}
-                                        article={article}
-                                        key={article._id}
-                                        />
-                                </TouchableOpacity>
-                            )
-                        })}
-                        </View>
+        <ScrollView>
+            <ImageBackground style={{width:"100%"}} source={require("../assets/fondoVioleta.png")} resizeMode="cover">
+                <HeroPages/>
+                <View style={styles.articlesMain}>
+                    <Text style={styles.articlesTitle}>Articles</Text>
+                    <Filter
+                        filterArticles={(e) => filterArticles(e)}
+                        currentPage={currentPage}
+                        setCurrentPage={setCurrentPage}
+                    /> 
+                    <View>
+                    {articles.articles.map((article) => {
+                        return (
+                            <TouchableOpacity onPress={() => props.navigation.navigate('ArticleStack', { id: article._id })}>
+                                <Article
+                                    history={props.history}
+                                    article={article}
+                                    key={article._id}
+                                    />
+                            </TouchableOpacity>
+                        )
+                    })}
                     </View>
-                    <Footer/>
-                </ImageBackground>
-            </ScrollView>
+                </View>
+                <Footer/>
+            </ImageBackground>
+        </ScrollView>
     )
 }
 
