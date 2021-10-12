c415d9e0ca3788bef079d86cfcbd0a3812e2b369
Jonathan Mardones <jmardones.94@gmail.com>
2021-10-12 08:52:42 -0300

    x

--- mobile/components/Filter.js
+++ mobile/components/Filter.js
@@ -1,98 +1,104 @@
 import React, { useState, useEffect } from "react"
-import {View, Text, TextInput, TouchableOpacity, StyleSheet} from "react-native"
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native"
 import { connect } from "react-redux"
 import articlesUtilitiesActions from "../redux/actions/articlesUtilitiesActions"
 import { useArticles, useFilters, useUtils } from "../hooks/articlesHooks"
-import CheckBox from '@react-native-community/checkbox'
+// import CheckBox from '@react-native-community/checkbox'
 
 const Filter = (props) => {
-    const [toggleCheckBox, setToggleCheckBox] = useState(false)
-    const [{ brands, genres, gameTypes }, loadingUtils, erroUtils] = useUtils()
-    const [submit, setSubmit] = useState(false)
-    const {
-       filters,
-       inputPrice,
-       inputBoolean,
-       inputHandle,
-       inputMinAge,
-       inputPlayers,
-    } = useFilters()
+  const [toggleCheckBox, setToggleCheckBox] = useState(false)
+  const [{ brands, genres, gameTypes }, loadingUtils, erroUtils] = useUtils()
+  const [submit, setSubmit] = useState(false)
+  const {
+    filters,
+    inputPrice,
+    inputBoolean,
+    inputHandle,
+    inputMinAge,
+    inputPlayers,
+  } = useFilters()
 
-    const [articles, loading, error] = useArticles(
-       filters,
-       submit,
-       props.currentPage
-    )
-   
-    useEffect(() => {
-        props.setLoadingArticles?.(loading)
-    }, [loading])
-  
-     useEffect(() => {
-        props.filterArticles(articles)
-    }, [articles])
-  
-    // const renderOptions = (options) => {
-    //     return options.map((option) => {
-    //        return (
-    //           <option key={option._id} value={option._id}>
-    //              {option.name}
-    //           </option>
-    //        )
-    //     })
-    // }
+  const [articles, loading, error] = useArticles(
+    filters,
+    submit,
+    props.currentPage
+  )
 
-    const checkboxHandler = (newValue) => {
-        setToggleCheckBox(newValue)
-        inputBoolean(newValue)
-    }
+  useEffect(() => {
+    props.setLoadingArticles?.(loading)
+  }, [loading])
 
-    const submitFilters = (e) => {
-        e.preventDefault()
-        props.setCurrentPage(1)
-        setSubmit(!submit)
-    }
+  useEffect(() => {
+    props.filterArticles(articles)
+  }, [articles])
 
-    return(
-        <View style={styles.filterUltracontainer}>
-            <View style={styles.filterBoxContainer}>
-                <TextInput placeholder="Search a product" style={styles.search} onChangeText={(e) => {inputHandle(e)}}/>
-                <View style={styles.filtersBox}>
+  // const renderOptions = (options) => {
+  //     return options.map((option) => {
+  //        return (
+  //           <option key={option._id} value={option._id}>
+  //              {option.name}
+  //           </option>
+  //        )
+  //     })
+  // }
 
-                </View>
-                <View style={styles.filterDiscount}>
-                    <Text>With discount</Text>
-                    <CheckBox
+  const checkboxHandler = (newValue) => {
+    setToggleCheckBox(newValue)
+    inputBoolean(newValue)
+  }
+
+  const submitFilters = (e) => {
+    e.preventDefault()
+    props.setCurrentPage(1)
+    setSubmit(!submit)
+  }
+
+  return (
+    <View style={styles.filterUltracontainer}>
+      <View style={styles.filterBoxContainer}>
+        <TextInput
+          placeholder="Search a product"
+          style={styles.search}
+          onChangeText={(e) => {
+            inputHandle(e)
+          }}
+        />
+        <View style={styles.filtersBox}></View>
+        <View style={styles.filterDiscount}>
+          <Text>With discount</Text>
+          {/* <CheckBox
                         value={toggleCheckBox}
                         onValueChange={(newValue) => checkboxHandler(newValue)}
-                    />
-                </View>
-                <TouchableOpacity onPress={(e) => {submitFilters(e)}}><Text>Filter</Text></TouchableOpacity>
-            </View>
+                    /> */}
         </View>
-    )
+        <TouchableOpacity
+          onPress={(e) => {
+            submitFilters(e)
+          }}
+        >
+          <Text>Filter</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
 }
 
 const mapDispatchToProps = {
-    getAllArticlesUtilities: articlesUtilitiesActions.getAllArticlesUtilities,
+  getAllArticlesUtilities: articlesUtilitiesActions.getAllArticlesUtilities,
 }
 
 export default connect(null, mapDispatchToProps)(Filter)
 
 const styles = StyleSheet.create({
-    filterUltracontainer: {
-
-    },
-    filterBoxContainer: {
-
-    },
-    search: {
-
-    },
-    filterDiscount: {
-
-    },
-    filtersBox: {
-
-    },
+  filterUltracontainer: {},
+  filterBoxContainer: {},
+  search: {},
+  filterDiscount: {},
+  filtersBox: {},
 })
