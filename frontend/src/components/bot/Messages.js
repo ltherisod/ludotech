ca4afca4d16d94a8f52dddcadac88086eeec2b14
Diego Iglesias <diegoiglesiasrojo@gmail.com>
2021-10-14 14:00:15 -0300

    messages on bot fixed 2

--- frontend/src/components/bot/Messages.js
+++ frontend/src/components/bot/Messages.js
@@ -1,12 +1,12 @@
 const Messages = [
-    {command: "payment methods", response: "the available payment methods are credit card, debit card and paypal", id: 1},
-    {command: "order", response: "when you make an order we will get in touch to confirm the delivery", id: 2},
-    {command: "profile", response: "you can click on profile to change your personal data", id: 3},
-    {command: "refund", response: "if you need to refund a purchase, get in touch with our team on ludotechweb@gmail.com", id: 4},
-    {command: "problems", response: "if you have a problem, get in touch with our team on ludotechweb@gmail.com", id: 5},
-    {command: "cancel", response: "if you need to cancel a purchase, get in touch with our team on ludotechweb@gmail.com", id: 6},
-    {command: "address", response: "to add a new delivery address clicks on settings - profile", id: 7},
-    {command: "purchase", response: "to see your list of purchases clicks on settings - purchases", id: 8},
+    {command: "payment methods", response: "The available payment methods are credit card, debit card and paypal", id: 1},
+    {command: "order", response: "When you make an order we will get in touch to confirm the delivery", id: 2},
+    {command: "profile", response: "You can click on profile to change your personal data", id: 3},
+    {command: "refund", response: "If you need to refund a purchase, get in touch with our team on ludotechweb@gmail.com", id: 4},
+    {command: "problems", response: "If you have a problem, get in touch with our team on ludotechweb@gmail.com", id: 5},
+    {command: "cancel", response: "If you need to cancel a purchase, get in touch with our team on ludotechweb@gmail.com", id: 6},
+    {command: "address", response: "To add a new delivery address clicks on settings - profile", id: 7},
+    {command: "purchase", response: "To see your list of purchases clicks on settings - purchases", id: 8},
 ]
 
 export default Messages
