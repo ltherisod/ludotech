2668af0661c8bf08692b1a45c7969eb9520d2cbd
Diego Iglesias <diegoiglesiasrojo@gmail.com>
2021-10-14 13:59:24 -0300

    messages on bot fixed

--- frontend/src/components/bot/Messages.js
+++ frontend/src/components/bot/Messages.js
@@ -1,12 +1,12 @@
 const Messages = [
-    {command: "payment methods", response: "the payment methods available are credit or debit card and paypal", id: 1},
-    {command: "order", response: "when you do an order we will get in touch to define delivery", id: 2},
+    {command: "payment methods", response: "the available payment methods are credit card, debit card and paypal", id: 1},
+    {command: "order", response: "when you make an order we will get in touch to confirm the delivery", id: 2},
     {command: "profile", response: "you can click on profile to change your personal data", id: 3},
     {command: "refund", response: "if you need to refund a purchase, get in touch with our team on ludotechweb@gmail.com", id: 4},
     {command: "problems", response: "if you have a problem, get in touch with our team on ludotechweb@gmail.com", id: 5},
     {command: "cancel", response: "if you need to cancel a purchase, get in touch with our team on ludotechweb@gmail.com", id: 6},
-    {command: "address", response: "to save a new delivery address clicks on settings - profile", id: 7},
-    {command: "purchase", response: "to see your list of purchases made clicks on settings - purchases", id: 8},
+    {command: "address", response: "to add a new delivery address clicks on settings - profile", id: 7},
+    {command: "purchase", response: "to see your list of purchases clicks on settings - purchases", id: 8},
 ]
 
 export default Messages
