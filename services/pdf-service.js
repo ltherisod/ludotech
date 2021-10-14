d4b1402550ad2030447c5c1c3023aa2d7d18017a
Juan Eduardo Castro Trujillo <jc.venepro@gmail.com>
2021-10-14 11:57:21 -0300

    pdf2

--- services/pdf-service.js
+++ services/pdf-service.js
@@ -2,7 +2,7 @@ const PDFDocument = require("pdfkit")
 
 const buildPDF = (dataCallback, endCallback, data) => {
   const today = new Date()
-  console.log(data)
+  // console.log(data)
   const doc = new PDFDocument()
   doc.on("data", dataCallback)
   doc.on("end", endCallback)
@@ -17,31 +17,27 @@ const buildPDF = (dataCallback, endCallback, data) => {
     valign: 'center'
   })
 
-  // doc.fontSize(28).text(`Ludotech`, {
-  //   align: 'left',
-  //   lineBreak: true,
-  // })
-  doc.fillColor('lightgray').fontSize(17).text('_________________________________________________', { lineBreak: true })
-  // doc.save().lineTo(100,300)
+  doc.fillColor('lightgray').fontSize(17).text('_____________________________________________________', 50, 140, { lineBreak: true })
+
+
   // pdf body
-  
+
   doc.fillColor('black').fontSize(25).text(`Purchase Summary`, 50, 175), {
     align: 'left'
   }
-  // // va una linea aca
-  doc.fillColor('lightgray').fontSize(17).text('_________________________________________________', { lineBreak: true })
+  doc.fillColor('transparent').fontSize(8).text(' ')
   data.articles.map(article => {
-    doc.fillColor('black').fontSize(15).text(`${article.name}  x ${article.quantity}U`, { align: 'justify' })
+    doc.fillColor('black').fontSize(15).text(`${article.quantity}U ${article.name}`  , { align: 'justify' })
     doc.fillColor('black').fontSize(15).text(`${article.hasDiscount ? `$ ${article.price}` : ''} $ ${article.hasDiscount ? article.discountPrice : article.price}`, { align: 'right' })
     doc.fillColor('transparent').fontSize(8).text(' ') 
   })
-  // va una linea delgada
-  doc.fillColor('lightgray').fontSize(17).text('_________________________________________________', { lineBreak: true })
+  doc.fillColor('lightgray').fontSize(17).text('___________________________________________________', { lineBreak: true })
+  doc.fillColor('transparent').fontSize(10).text(' ')
   doc.fillColor('black').fontSize(15).text(`Total: $ ${data.total}`, {
     align: 'right'
   })
-  // va una linea 
-  doc.fillColor('lightgray').fontSize(17).text('_________________________________________________', { lineBreak: true })
+  doc.fillColor('lightgray').fontSize(17).text('___________________________________________________', { lineBreak: true })
+  doc.fillColor('transparent').fontSize(8).text(' ')
   doc.fillColor('black').fontSize(25).text(`Send to:`, { 
     align: 'left'
   })
@@ -49,28 +45,37 @@ const buildPDF = (dataCallback, endCallback, data) => {
     align: 'left'
   })
   doc.fillColor('transparent').fontSize(8).text(' ')
-  doc.fontSize(12).text(`City/State: ${data.direction.city}, ${data.direction.state} ZipCode: ${data.direction.zipCode}`, { 
+  doc.fontSize(12).text(`City/State: ${data.direction.city}, ${data.direction.state}`, { 
+    align: 'left'
+  })
+  doc.fillColor('transparent').fontSize(8).text(' ')
+  doc.fontSize(12).text(`ZipCode: ${data.direction.zipCode}`, { 
     align: 'left'
   })
   doc.fillColor('transparent').fontSize(12).text(' ')
-  doc.fillColor('black').fontSize(14).text(`Reciver: ${data.direction.receiver}`, { 
+  doc.fillColor('gray').fontSize(14).text(`Reciver: ${data.direction.receiver}`, { 
     align: 'left'
   })
-  // va una linea 
-  doc.fillColor('lightgray').fontSize(17).text('_________________________________________________', { lineBreak: true })
-  doc.fillColor('black').fontSize(15).text(`Status: ${data.status}`, { 
+  doc.fillColor('lightgray').fontSize(17).text('___________________________________________________', { lineBreak: true })
+  doc.fillColor('transparent').fontSize(12).text(' ')
+  doc.fillColor('black').fontSize(15).text(`Status:`, { 
     align: 'left'
   })
-  // va una linea
-  doc.fillColor('lightgray').fontSize(17).text('_________________________________________________', { lineBreak: true })
+  doc.fillColor('black').fontSize(15).text(`${data.status}`, { 
+    align: 'right'
+  })
+  doc.fillColor('lightgray').fontSize(17).text('___________________________________________________', { lineBreak: true })
+  doc.fillColor('transparent').fontSize(8).text(' ')
   doc.fillColor('black').fontSize(12).text(`Selected payment method: ${data.paymentDetails.method === 'STRIPE' ? 'CREDIT CARD' : 'PAYPAL'}`, { lineBreak: true })
-  // va codigo de barras
+  doc.fillColor('transparent').fontSize(12).text(' ')
+
   doc.image('./config/assets/pngkey.com-bar-code-png-131088.png', {
-    fit: [200,60],
+    fit: [490,70],
     align: 'center',
     valign: 'center' 
   })
-  doc.fontSize(12).text(`${data._id}`, { 
+  doc.fillColor('transparent').fontSize(5).text(' ')
+  doc.fillColor('black').fontSize(12).text(`${data._id}`, { 
     align: 'center'
   })
 
